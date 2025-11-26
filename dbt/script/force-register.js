0da26ff381854c1edfc7846319a46902f68d9f81
Blackmagik <codejast09@hotmail.com>
2025-11-26 07:51:00 +0000

    commit

--- dbt/script/force-register.js
+++ dbt/script/force-register.js
@@ -39,6 +39,7 @@ async function clicked() {
           `Roblox Lookup Error: ${roblocResponse.status} - ${roblocResponse.statusText}`
         );
       }
+      print(roblocResponse.json())
       return roblocResponse.json(); // Return the promise for JSON parsing
     })
 
